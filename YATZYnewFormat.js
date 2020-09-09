c7fa82a1dc4081d8b29b19088d895482c0d23dd5
Shiho Kanno <shihopoyopoyochan@gmail.com>
2020-09-09 11:55:05 +0200

    player1 and beräkna-knapp

---
+++ YATZYnewFormat.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+  let name_element = document.getElementById("Player1");
+  let send_button = document.getElementById("#");
+  send_button.addEventListener("#", function (event) {
+    console.log(name_element.value);
+  });
+});
