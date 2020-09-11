5782691b720a9c3bd71c9ef5719310c61cfb5da6
Shiho Kanno <shihopoyopoyochan@gmail.com>
2020-09-11 18:14:15 +0200

    Upperbox done withoutbutton

--- YATZYnewFormat.js
+++ YATZYnewFormat.js
@@ -1,7 +1,50 @@
 // JavaScriptがHTMLに尋ねて、準備ができたか返事を得る。
+
+function loopinput() {
+  let sum = 0;
+  var Player1Inputs = document.getElementsByClassName("upPlayer1");
+  for (let input of Player1Inputs) {
+    sum += parseInt(input.value);
+  }
+
+  if (sum > 63) {
+    sum += 50;
+    let bonus;
+    document.querySelector("#player1Bonus").innerHTML = 50;
+  }
+  document.querySelector("#player1Sum").innerHTML = sum;
+}
+
 document.addEventListener("DOMContentLoaded", function (event) {
+  //let calc_button = document.querySelectorAll("#Summa1");
+
+  var OnesPlayer1 = document.getElementById("OnesPlayer1");
+  OnesPlayer1.addEventListener("change", function (event) {
+    loopinput();
+  });
+  var TowsPlayer1 = document.getElementById("TowsPlayer1");
+  TowsPlayer1.addEventListener("change", function (event) {
+    loopinput();
+  });
+  var threesPlayer1 = document.getElementById("threesPlayer1");
+  threesPlayer1.addEventListener("change", function (event) {
+    loopinput();
+  });
+  var FoursPlayer1 = document.getElementById("FoursPlayer1");
+  FoursPlayer1.addEventListener("change", function (event) {
+    loopinput();
+  });
+  var FivesPlayer1 = document.getElementById("FivesPlayer1");
+  FivesPlayer1.addEventListener("change", function (event) {
+    loopinput();
+  });
+  var SixesPlayer1 = document.getElementById("SixesPlayer1");
+  SixesPlayer1.addEventListener("change", function (event) {
+    loopinput();
+  });
+
   let calc_button = document.querySelectorAll("#Summa1");
-  calc_button[0].addEventListener("click", function (event) {
+  calc_button[0].addEventListener("chenge", function (event) {
     //HTMLからボックスの値を文字で取ってくる。querySelect...のときは、(.名前)をする。CSSも。配列ができた。
     let player1Elements = document.querySelectorAll(".upPlayer1");
     console.log(player1Elements);
@@ -22,7 +65,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     //ボーナスをチェック。
     if (player1Sum > 63) {
-      let player1bonus = 30;
+      let player1bonus = 50;
       console.log(player1bonus);
       //HTMLから一つ要素を取ってきてHTML内の空欄部分に表示する。
       document.querySelector("#player1Bonus").innerHTML = player1bonus;
