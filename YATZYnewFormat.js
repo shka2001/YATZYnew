799ca6b2462e1d3c8bf560f9cb37c47518f0b842
Shiho Kanno <shihopoyopoyochan@gmail.com>
2020-09-11 19:30:44 +0200

    Plaryer1 down and up calc done

--- YATZYnewFormat.js
+++ YATZYnewFormat.js
@@ -1,24 +1,28 @@
-// JavaScriptがHTMLに尋ねて、準備ができたか返事を得る。
-
-function loopinput() {
-  let sum = 0;
-  var Player1Inputs = document.getElementsByClassName("upPlayer1");
-  for (let input of Player1Inputs) {
-    sum += parseInt(input.value);
-  }
-
-  if (sum > 63) {
-    sum += 50;
-    let bonus;
-    document.querySelector("#player1Bonus").innerHTML = 50;
-  }
-  document.querySelector("#player1Sum").innerHTML = sum;
-}
-
+// JavaScriptがHTMLに尋ねる。
 document.addEventListener("DOMContentLoaded", function (event) {
-  //let calc_button = document.querySelectorAll("#Summa1");
+  //入力された数字を取る。
 
-  var OnesPlayer1 = document.getElementById("OnesPlayer1");
+  function loopinput() {
+    let sum = 0;
+    var upPlayer1Inputs = document.getElementsByClassName("upPlayer1");
+    for (let input of upPlayer1Inputs) {
+      sum += parseInt(input.value);
+    }
+
+    if (sum > 63) {
+      sum += 50;
+      document.querySelector("#player1Bonus").innerHTML = 50;
+    }
+    document.querySelector("#player1Sum").innerHTML = sum;
+  }
+
+  let upPlayer1InputNumbers = document.getElementsByClassName("upPlayer1");
+  for (let upPlayer1InputNumbersArr of upPlayer1InputNumbers) {
+    upPlayer1InputNumbersArr.addEventListener("change", function (event) {
+      loopinput();
+    });
+  }
+  /*var OnesPlayer1 = document.getElementById("OnesPlayer1");
   OnesPlayer1.addEventListener("change", function (event) {
     loopinput();
   });
@@ -41,41 +45,26 @@ document.addEventListener("DOMContentLoaded", function (event) {
   var SixesPlayer1 = document.getElementById("SixesPlayer1");
   SixesPlayer1.addEventListener("change", function (event) {
     loopinput();
-  });
+  });'´*/
 
-  let calc_button = document.querySelectorAll("#Summa1");
-  calc_button[0].addEventListener("chenge", function (event) {
-    //HTMLからボックスの値を文字で取ってくる。querySelect...のときは、(.名前)をする。CSSも。配列ができた。
-    let player1Elements = document.querySelectorAll(".upPlayer1");
-    console.log(player1Elements);
+  function calcDownBox() {
+    let total = 0;
+    let upSum = parseInt(document.getElementById("player1Sum").innerHTML);
 
-    //配列のボックスを作って文字を数字にし全て終えるまでループする。
-    let player1Numbers = [0, 0, 0, 0, 0, 0];
-    for (let index = 0; index < player1Numbers.length; index++) {
-      //文字を数字にする。
-      player1Numbers[index] = parseInt(player1Elements[index].value);
-    }
-    //数字化した配列を合計する。
-
-    let player1Sum = 0;
-    for (let index2 = 0; index2 < player1Numbers.length; index2++) {
-      player1Sum += player1Numbers[index2];
-      console.log(player1Sum);
+    var downPlayer1Inputs = document.getElementsByClassName("downPlayer1");
+    for (let downInput of downPlayer1Inputs) {
+      upSum += parseInt(downInput.value);
     }
 
-    //ボーナスをチェック。
-    if (player1Sum > 63) {
-      let player1bonus = 50;
-      console.log(player1bonus);
-      //HTMLから一つ要素を取ってきてHTML内の空欄部分に表示する。
-      document.querySelector("#player1Bonus").innerHTML = player1bonus;
-      document.querySelector("#player1Sum").innerHTML =
-        player1Sum + player1bonus;
-    } else {
-      let player1bonus = 0;
-      document.querySelector("#player1Bonus").innerHTML = player1bonus;
-      document.querySelector("#player1Sum").innerHTML =
-        player1Sum + player1bonus;
-    }
-  });
+    total += upSum;
+    console.log(total);
+    document.querySelector("#player1Total").innerHTML = total;
+  }
+
+  let downPlayer1InputNumbers = document.getElementsByClassName("downPlayer1");
+  for (let downPlayer1InputNumbersArr of downPlayer1InputNumbers) {
+    downPlayer1InputNumbersArr.addEventListener("change", function (event) {
+      calcDownBox();
+    });
+  }
 });
